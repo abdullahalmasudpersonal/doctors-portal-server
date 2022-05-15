d74a65a04c18b80fa5a67d0ad14edd14acf8e261
masud <abdullahalmasud01737906772@gmail.com>
2022-05-15 20:09:18 +0600

    another commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -14,12 +15,27 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+/* function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'UnAuthorized access'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+    if(err){
+      return res.status(403).send({message: 'Forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+  });
+} */
+
 async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db('doctors_portal').collection('services');
     const bookingCollection = client.db('doctors_portal').collection('bookings');
-
+    const userCollection = client.db('doctors_portal').collection('users');
 
     app.get('/service', async (req, res) => {
       const query = {};
@@ -28,21 +44,30 @@ async function run() {
       res.send(services);
     });
 
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({result, token});
+    })
+
     /// worning
     //// This is not the proper way to query.
     //// after learing more about mongodb. use aggregate lookup, popeline, match, group
     app.get('/available', async (req, res) => {
       const date = req.query.date;
 
-
       // step 1: get all services
-
       const services = await serviceCollection.find().toArray();
-
       // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}]
       const query = { date: date };
       const bookings = await bookingCollection.find(query).toArray();
-
       // step 3: for each service
       services.forEach(service => {
         // step 4: find bnookings for that service. output: [{}, {}, {}, {}, {}]
@@ -54,7 +79,7 @@ async function run() {
         // step 7: set available to slots to make it easier
         service.slots = available;
       })
- 
+
       res.send(services);
     })
 
@@ -67,13 +92,16 @@ async function run() {
      * app.delete('/booking/:id') //
      * **/
 
-    app.get('/booking', async(req, res)=>{
+    app.get('/booking', async (req, res) => {
       const patient = req.query.patient;
-      const query = {patient: patient};
-      const bookings = await bookingCollection.find(query).toArray();
-      res.send(bookings);
-    })
+      const authorization = req.headers.authorization;
+      console.log('auth header', authorization);
+        const query = { patient: patient };
+        const bookings = await bookingCollection.find(query).toArray();
+        return res.send(bookings);
+      
 
+    })
 
     app.post('/booking', async (req, res) => {
       const booking = req.body;
@@ -93,7 +121,6 @@ async function run() {
 }
 
 run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
   res.send('Hello World Doctor Uncle!')
